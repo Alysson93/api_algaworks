06f8a0945c8ff02c59b67ad15bf6981816672c0d
Alysson Pereira | Grupo Moura <alysson.pereira@grupomoura.com>
2023-10-31 14:25:22 -0300

    get and post

---
+++ src/main/java/com/example/api_algaworks/controllers/ClientController.java
@@ -0,0 +1,32 @@
+package com.example.api_algaworks.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.api_algaworks.models.Client;
+import com.example.api_algaworks.repositories.ClientRepository;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+    
+    @GetMapping()
+    public List<Client> listar() {
+        return clientRepository.findAll();
+    }
+
+    @PostMapping()
+    public Client adicionar(@RequestBody Client client) {
+        return clientRepository.save(client);
+    }
+
+}
---
+++ src/test/java/com/example/api_algaworks/ApiAlgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_algaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAlgaworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
